6d324a149b039156cebbbb15af9c6a8ec18dff0c
takeyama-h1 <h-takeyama@usen-next.jp>
2021-08-05 08:21:24 +0000

    prのurlをoutputする

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 set -eu
-git-pr-release --squashed
+git-pr-release --squashed | tee result.txt
+
+PR_URL=$(grep -E "https://github.com/(.*)/pull/(.*)" result.txt | sed -r 's#(.*)(https://github.com/.*/pull/[0-9])#\2#')
+echo "::set-output name=pr_url::$PR_URL"
