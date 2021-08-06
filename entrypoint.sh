4892a730ae07ec9456c11882a8188ccef70aa071
takeyama-h1 <h-takeyama@usen-next.jp>
2021-08-06 06:59:08 +0000

    add no_pr output param

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,9 @@ git-pr-release --squashed 2>&1 | tee result.txt
 PR_URL=$(grep -E "https://github.com/(.*)/pull/(.*)" result.txt | sed -r 's#^(.*)(https://github.com/.*/pull/[0-9]*)(.*)$#\2#')
 echo "pr_url: $PR_URL"
 echo "::set-output name=pr_url::$PR_URL"
+
+if grep -q "No pull requests to be released" result.txt; then
+    echo "::set-output name=no_pr::true"
+else
+    echo "::set-output name=no_pr::false"
+fi
